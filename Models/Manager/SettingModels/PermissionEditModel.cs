a6647b2e17f4e68cec465d4d294a3a4c7e61e01b
Håkan Edling <hakan@tidyui.com>
2012-04-16 15:51:26 +0200

    Some more models.
    Added list & edit models for permissions.

--- Models/Manager/SettingModels/PermissionEditModel.cs
+++ Models/Manager/SettingModels/PermissionEditModel.cs
@@ -9,55 +9,43 @@ using Piranha.Entities;
 
 namespace Piranha.Models.Manager.SettingModels
 {
-	/// <summary>
-	/// Group edit model for the manager area.
-	/// </summary>
-	public class GroupEditModel
+	public class PermissionEditModel
 	{
 		#region Properties
 		/// <summary>
-		/// Gets/sets the group.
+		/// Gets/sets the current access record.
 		/// </summary>
-		public Group Group { get ; set ; }
-		
-		/// <summary>
-		/// Gets/sets of all of the other groups.
-		/// </summary>
-		public SelectList Groups { get ; set ; }
+		public Permission Permission { get ; set ; }
 
 		/// <summary>
-		/// Gets/sets the members of the group.
+		/// Gets/sets the available groups.
 		/// </summary>
-		public List<User> Members { get ; set ; }
+		public SelectList Groups { get ; set ; }
 		#endregion
 
 		/// <summary>
 		/// Default constructor. Creates a new model.
 		/// </summary>
-		public GroupEditModel() {
-			Group = new Group() ;
-	
+		public PermissionEditModel() {
+			Permission = new Permission() ;
+
 			using (var db = new Data.PiranhaContext()) {
-				var groups = db.Groups.OrderBy(g => g.Name).Select(g => new Group() { Id = g.Id, Name = g.Name }).ToList() ;
+				var groups = db.Groups.GetFlattenedStructure() ;
 				groups.Insert(0, new Group() { Name = "" }) ;
 				Groups = new SelectList(groups, "Id", "Name") ;
 			}
 		}
 
 		/// <summary>
-		/// Gets the model for the specified group.
+		/// Gets the specified access model.
 		/// </summary>
-		/// <param name="id">The id</param>
+		/// <param name="id">The access id</param>
 		/// <returns>The model</returns>
-		public static GroupEditModel GetById(Guid id) {
-			var m = new GroupEditModel() ;
+		public static PermissionEditModel GetById(Guid id) {
+			var m = new PermissionEditModel() ;
 
 			using (var db = new Data.PiranhaContext()) {
-				var groups = db.Groups.Where(g => g.Id != id).OrderBy(g => g.Name).Select(g => new Group() { Id = g.Id, Name = g.Name }).ToList() ;
-				groups.Insert(0, new Group() { Name = "" }) ;
-				m.Groups = new SelectList(groups, "Id", "Name") ;
-				m.Group = db.Groups.Where(g => g.Id == id).Single() ;
-				m.Members = db.Users.Where(u => u.GroupId == id).ToList() ;
+				m.Permission = db.Permissions.Where(p => p.Id == id).Single() ;
 			}
 			return m ;
 		}
@@ -68,7 +56,7 @@ namespace Piranha.Models.Manager.SettingModels
 		/// <returns>Weather the action succeeded or not.</returns>
 		public bool SaveAll() {
 			using (var db = new Data.PiranhaContext()) {
-				Group.Attach(db) ;
+				Permission.Attach(db) ;
 				return db.SaveChanges() > 0 ;
 			}
 		}
@@ -79,7 +67,7 @@ namespace Piranha.Models.Manager.SettingModels
 		/// <returns>Weather the action succeeded or not.</returns>
 		public bool DeleteAll() {
 			using (var db = new Data.PiranhaContext()) {
-				Group.Attach(db, EntityState.Deleted) ;
+				Permission.Attach(db, EntityState.Deleted) ;
 				return db.SaveChanges() > 0 ;
 			}
 		}
